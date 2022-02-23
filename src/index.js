82c124284e5a9fa68fd6dd3bc0608efbeffa952f
warren yun <wyun2006@gmail.com>
2022-02-22 19:49:00 -0500

    server template

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config()
+
+const app = express();
+const port = process.env.PORT || 5000;
+const uri = process.env.ATLAS_URI;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(uri, { useNewUrlParser: true, });
+mongoose.connection.once('open', () => {
+   console.log("connected to mongodb") 
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});

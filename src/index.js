402073c3272423d0fa496e7fbd022f4cc1f95e39
warren yun <wyun2006@gmail.com>
2022-02-23 10:51:08 -0500

    pain

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 require('dotenv').config()
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 const uri = process.env.ATLAS_URI;
 
 app.use(cors());

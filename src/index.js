512ecba749a9101e2d68470eb2eb53e1d8cb41be
warren yun <wyun2006@gmail.com>
2022-02-23 10:31:50 -0500

    ok maybe not

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 require('dotenv').config()
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 const uri = process.env.ATLAS_URI;
 
 app.use(cors());
